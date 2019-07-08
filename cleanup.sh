3c06d91c85402e455aebd022f1040d2bf1fbc86f
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-07-07 19:22:23 -0500

    cleanup

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf *.txt *.out
+rm -rf data/*.txt *.out
+rm -rf data_java/*.txt *.out
+rm -rf data_java/*/*.txt *.out
+rm -rf data_java/*/*/*.txt *.out
+rm -rf data_java/*/*/dataset
+
